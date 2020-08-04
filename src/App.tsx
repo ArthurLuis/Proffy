fee988f8a222eab79f8e11ee5494441eec1ce0e0
Gerardo Majella <alfa@cin.ufpe.br>
2020-08-04 01:02:15 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './assets/styles/global.css'
+import './assets/pages/Landing'
+import Landing from './assets/pages/Landing'
+
+function App() {
+  return <Landing />
+}
+
+export default App
